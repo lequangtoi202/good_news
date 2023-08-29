e966ce64130a37dd6ea9aa2ca6b6c7b6f7ec7497
lequangtoi202 <2051052140toi@ou.edu.vn>
2023-08-29 20:37:19 +0700

    init project 29-08

---
+++ src/main/java/com/quangtoi/good_news/GoodNewsApplication.java
@@ -0,0 +1,13 @@
+package com.quangtoi.good_news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodNewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quangtoi/good_news/GoodNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quangtoi.good_news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodNewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
